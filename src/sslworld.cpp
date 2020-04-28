d9cf80625d6e0f652deb2ea44423d9721cbee9c5
goncamateus <mgm4@cin.ufpe.br>
2020-04-28 01:24:11 -0300

    fixes timer when it's a goal

--- src/sslworld.cpp
+++ src/sslworld.cpp
@@ -717,9 +717,12 @@ void SSLWorld::posProcess()
         goals_yellow++;
         is_goal = true;
     }
+    // if(bx < -0.6 && abs(by < 0.35))
+
     time_before = time_after;
     time_after = timer->elapsed()/300000;
-    if (is_goal || time_after != time_before)
+    bool end_time = time_after != time_before;
+    if (is_goal || end_time)
     {
         float LO_X = -0.65;
         float LO_Y = -0.55;
@@ -737,6 +740,8 @@ void SSLWorld::posProcess()
                 continue;
             robots[i]->setXY(x, y);
         }
+        timer->restart();
+        time_before = time_after = 0;
     }
 }
 
